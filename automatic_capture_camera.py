a059de275d5fb029c157af12aa97cf337048e0cf
Serge <sergelouienebrao@gmail.com>
2025-01-24 22:35:17 +0800

    multiface detection

--- automatic_capture_camera.py
+++ automatic_capture_camera.py
@@ -4,17 +4,20 @@ import datetime
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(r'C:/Desktop/mycodes/.venv/Lib/site-packages/haarcascade_frontalface_default.xml')
 smile_cascade = cv2.CascadeClassifier(r'C:/Downloads/haarcascade_smile.xml')
+
 while True:
      _, frame = cap.read()
      original_frame = frame.copy()
      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-     face = face_cascade.detectMultiScale(gray, 1.3, 5)
-     for x, y, width, height in face:
+     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 5, minSize = (100, 100))
+
+     for x, y, width, height in faces:
         cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 225, 255), 2) 
         face_roi = frame[y: y + height, x: x + width]
         gray_roi = gray[y: y + height, x: x + width]
-        smile = smile_cascade.detectMultiScale(gray_roi, 1.3, 25)
-        for x1, y1, width1, height1 in smile:
+        smiles = smile_cascade.detectMultiScale(gray_roi, scaleFactor = 1.8, minNeighbors = 25, minSize= (25, 25))
+        
+        for x1, y1, width1, height1 in smiles:
             cv2.rectangle(face_roi, (x1, y1), (x1 + width1, y1 + height1), (0, 0, 255), 2)
             time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
             file_name = f"selfie-{time_stamp}.png"
