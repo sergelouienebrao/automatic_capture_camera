8a8b33f7eb558985b1c80f933e7f8286ce9d5625
Serge <sergelouienebrao@gmail.com>
2025-01-26 01:15:13 +0800

    countdown and interval

--- automatic_capture_camera.py
+++ automatic_capture_camera.py
@@ -25,9 +25,18 @@ while True:
 
         for x1, y1, width1, height1 in smiles:
             cv2.rectangle(face_roi, (x1, y1), (x1 + width1, y1 + height1), (0, 0, 255), 2)
+            current_time = time.time()
+            if current_time - last_capture_time >= capture_delay:
+               for i in range(3, 0, -1):
+                  countdown_frame = frame.copy()
+                  cv2.putText(countdown_frame, f"Capturing in {i}", (50, 50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 255), 2)
+                  cv2.imshow("automatic capture camera", countdown_frame)
+                  cv2.waitKey(1000)
+
             time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
             file_name = f"selfie-{time_stamp}.png"
             cv2.imwrite(file_name, original_frame) 
+    
      cv2.imshow("automatic capture camera", frame)
      if cv2.waitKey(10) == ord("q"): 
       break 
