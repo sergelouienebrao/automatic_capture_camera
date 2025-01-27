16bde995f32010deefbc1cee3703c194f4542839
Serge <sergelouienebrao@gmail.com>
2025-01-27 17:14:02 +0800

    ewan ko na

--- automatic_capture_camera.py
+++ automatic_capture_camera.py
@@ -1,4 +1,9 @@
-#modification start
+# modification start
+# countdown
+# flipped cam
+# multiface detection
+
+
 import cv2
 import datetime
 import time
@@ -10,11 +15,14 @@ smile_cascade = cv2.CascadeClassifier(r'C:/Downloads/haarcascade_smile.xml')
 
 last_capture_time = 0
 capture_delay = 3
+countdown_start_time = None
 
 while True:
-     _, frame = cap.read()
+     ret, frame =cap.read()
+     if not ret:
+      break
+   
      frame = cv2.flip(frame, 1)
-     original_frame = frame.copy()
      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
      faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 5, minSize = (100, 100))
 
@@ -24,22 +32,26 @@ while True:
         gray_roi = gray[y: y + height, x: x + width]
         smiles = smile_cascade.detectMultiScale(gray_roi, scaleFactor = 1.8, minNeighbors = 25, minSize= (25, 25))
 
-        for x1, y1, width1, height1 in smiles:
-            cv2.rectangle(face_roi, (x1, y1), (x1 + width1, y1 + height1), (0, 0, 255), 2)
-            current_time = time.time()
+        if len(smiles) > 0:
+           current_time = time.time()
+           if countdown_start_time is None:
+              countdown_start_time = current_time 
 
-            if current_time - last_capture_time >= capture_delay:
-               for i in range(3, 0, -1):
-                  countdown_frame = frame.copy()
-                  cv2.putText(countdown_frame, f"Capturing in {i}", (50, 50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 255), 2)
-                  cv2.imshow("automatic capture camera", countdown_frame)
-                  cv2.waitKey(1000)
+           elapsed_time = current_time - countdown_start_time
+           countdown_time = capture_delay - int(elapsed_time)
 
+           if countdown_time > 0:
+            cv2.putText(frame, f"Capturing in {countdown_time}", (50, 50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 255), 2)
+           else: 
             time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
             file_name = f"selfie-{time_stamp}.png"
+            cv2.imwrite(file_name, frame)
             last_capture_time = current_time
-            cv2.imwrite(file_name, original_frame) 
-    
+            countdown_start_time = None
+         
+        else:
+            countdown_start_time = None
+
      cv2.imshow("automatic capture camera", frame)
      if cv2.waitKey(10) == ord("q"): 
       break 
